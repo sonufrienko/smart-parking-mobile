477151334fa790ea3a1a765a78f6c479d57c43eb
Sergey Onufrienko <sergey@onufrienko.com>
2019-08-07 16:50:52 +0300

    Add empty screens

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
---
+++ src/screens/Park.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Park Screen</Text>
+    </View>
+  );
+}
