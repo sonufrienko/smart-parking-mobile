335cbd8131c9d3f80b35705c6a0993e28b6c6475
Sergey Onufrienko <sergey@onufrienko.com>
2019-08-12 14:17:13 +0300

    Add start parking

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,36 @@
 export const SELECTED_PARKING = 'SELECTED_PARKING';
 export const CLEAR_SELECTED_PARKING = 'CLEAR_SELECTED_PARKING';
 
+export const START_PARKING_PENDING = 'START_PARKING_PENDING';
+export const START_PARKING_SUCCESS = 'START_PARKING_SUCCESS';
+export const START_PARKING_FAILURE = 'START_PARKING_FAILURE';
+
 export const selectParking = (parkingId) => {
   return {
     type: SELECTED_PARKING,
     parkingId
+  }
+}
+
+export const startParking = ({ vehicleId, plateNumber, slotNumber }) => {
+  return (dispatch, getState) => {
+    dispatch({
+       type: START_PARKING_PENDING 
+    });
+
+    setTimeout(() => {
+      const { user, map: { selectedParkingId } } = getState();
+
+      dispatch({
+        type: START_PARKING_SUCCESS,
+        invoice: {
+          vehicleId,
+          plateNumber,
+          slotNumber,
+          userID: user.id,
+          parkingId: selectedParkingId
+        }
+      });
+      }, 3000);
   }
 }
--- src/reducers/parkingReducer.ts
+++ src/reducers/parkingReducer.ts
@@ -1,3 +1,9 @@
+import { 
+  START_PARKING_PENDING,
+  START_PARKING_SUCCESS,
+  START_PARKING_FAILURE
+} from '../actions';
+
 const initialState = {
   history: [],
   activeParking: null,
@@ -7,6 +13,24 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case START_PARKING_PENDING:
+      return {
+        ...state,
+        loadingStartParking: true
+      }
+
+    case START_PARKING_SUCCESS:
+      return {
+        ...state,
+        activeParking: action.invoice,
+        loadingStartParking: false
+      }
+
+    case START_PARKING_FAILURE:
+      return {
+        ...state,
+        loadingStartParking: false
+      }
 
     default:
       return state;
