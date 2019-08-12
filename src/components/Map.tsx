5a8b100aa3791d1111bd58db5e019e61a23491d2
Sergey Onufrienko <sergey@onufrienko.com>
2019-08-12 14:51:41 +0300

    Display parkings as buttons from a state

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,29 +1,72 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, ScrollView, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 import { ScreenProps, withNavigation } from 'react-navigation';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const styles = {
-  screen: {
+const styles = StyleSheet.create({
+  container: {
     backgroundColor: '#ccc',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  marker: {
+    marginBottom: 10
   }
-};
+});
 
-function Map(props: ScreenProps) {
-  const { navigation } = props;
+type Parking = {
+  id: number,
+  title: string
+}
 
-  const parking = {
-    id: 1001,
-    title: 'Amsterdam Central Parking'
-  };
+type MapProps = {
+  parkingList: Parking[],
+  onParkingSelect(parking: Parking): void
+}
 
+type MapMarkerProps = {
+  parking: Parking,
+  onParkingSelect(parking: Parking): void
+}
+
+function MapMarker({ parking, onParkingSelect }: MapMarkerProps) {
   return (
-    <View style={styles.screen}>
-      <Button title="Park here" onPress={() => navigation.navigate('ParkingDetails', { parking })} />
+    <View style={styles.marker}>
+      <Button
+        title={parking.title} 
+        onPress={() => onParkingSelect(parking)} />
     </View>
   );
 }
 
-export default withNavigation(Map);
+function Map({ parkingList, onParkingSelect }: MapProps) {
+  return (
+    <View style={styles.container}>
+      {parkingList.map(parking => <MapMarker key={parking.id} parking={parking} onParkingSelect={onParkingSelect} />)}
+    </View>
+  );
+}
+
+function MapContainer(props: ScreenProps) {
+  const { navigation, map: { parkingList }, selectParking } = props;
+
+  return <Map 
+    parkingList={parkingList} 
+    onParkingSelect={parking => {
+      selectParking(parking.id);
+      navigation.navigate('ParkingDetails');
+    }} 
+  />
+}
+
+const mapStateToProps = ({ map }) => ({ map });
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  selectParking: (parkingId) => {
+    dispatch(actions.selectParking(parkingId));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withNavigation(MapContainer));
