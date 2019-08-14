231b00763fa9bbce416f0b50fbdca14efe78f701
Sergey Onufrienko <sergey@onufrienko.com>
2019-08-14 09:05:25 +0300

    Add a request to API

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,6 @@
+import { API, graphqlOperation } from 'aws-amplify';
+import * as mutations from '../graphql/mutations';
+
 export const SELECTED_PARKING = 'SELECTED_PARKING';
 export const CLEAR_SELECTED_PARKING = 'CLEAR_SELECTED_PARKING';
 
@@ -19,29 +22,65 @@ type StartParking = {
   navigation: any
 }
 
+type StartParkingInput = {
+  UserID: string,
+  PlateNumber: string,
+  SlotNumber: string
+};
+
+type StartParkingMutation = {
+  startParking:  {
+    Id: string,
+    UserID: string | null,
+    PlateNumber: string | null,
+    DateFrom: number | null,
+    DateTo: number | null,
+    Price: number | null,
+  } | null,
+};
+
 export const startParking = ({ vehicleId, plateNumber, slotNumber, navigation }: StartParking) => {
-  return (dispatch, getState) => {
+  return async (dispatch, getState) => {
     dispatch({
        type: START_PARKING_PENDING 
     });
 
-    setTimeout(() => {
-      const { account: { user }, map: { selectedParkingId } } = getState();
+    const { 
+      account: { user }, 
+      map: { selectedParkingId } 
+    } = getState();
+    
+    try {
+      const input: StartParkingInput = {
+        UserID: user.id,
+        PlateNumber: plateNumber,
+        SlotNumber: slotNumber
+      };
+      
+      const response = await API.graphql(graphqlOperation(mutations.startParking, { input }));
+      const data: StartParkingMutation = response.data;
+      const invoice = data.startParking;
+      console.log(invoice);
 
       dispatch({
         type: START_PARKING_SUCCESS,
         invoice: {
-          vehicleId,
-          plateNumber,
+          invoiceId: invoice.Id,
+          dateFrom: invoice.DateFrom,
+          dateTo: invoice.DateTo,
+          plateNumber: invoice.PlateNumber,
+          price: invoice.Price,
+          userID: invoice.UserID,
           slotNumber,
-          userID: user.id,
+          vehicleId,
           parkingId: selectedParkingId
         }
       });
 
       navigation.popToTop();
       navigation.navigate('ParkingHome');
-
-    }, 3000);
+    } catch(err) {
+      console.log(err);
+    }
   }
 }
