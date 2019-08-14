ec5c26eba0d0760b0815c9d4f388e615aeaf11be
Sergey Onufrienko <sergey@onufrienko.com>
2019-08-14 14:30:20 +0300

    Add Map to home screen

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,50 +1,90 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, Button, StyleSheet } from 'react-native';
 import { ScreenProps, withNavigation } from 'react-navigation';
+import MapView, { Marker } from 'react-native-maps';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ccc',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  marker: {
-    marginBottom: 10
+  statusBar: {
+    position: 'absolute',
+    width: '100%',
+    height: Constants.statusBarHeight,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)'
   }
-});
+})
 
 type Parking = {
   id: number,
   title: string
+  location: {
+    latitude: number,
+    longitude: number
+  }
 }
 
 type MapProps = {
   parkingList: Parking[],
-  onParkingSelect(parking: Parking): void
-}
-
-type MapMarkerProps = {
-  parking: Parking,
-  onParkingSelect(parking: Parking): void
-}
-
-function MapMarker({ parking, onParkingSelect }: MapMarkerProps) {
-  return (
-    <View style={styles.marker}>
-      <Button
-        title={parking.title} 
-        onPress={() => onParkingSelect(parking)} />
-    </View>
-  );
+  onParkingSelect(e: {
+    nativeEvent: {
+      id: string
+    }
+  }): void
 }
 
 function Map({ parkingList, onParkingSelect }: MapProps) {
+  const amsterdamLocation = {
+    latitude: 52.3783,
+    longitude: 4.8955,
+    latitudeDelta: 0.0212,
+    longitudeDelta: 0.0172
+  };
+
+  const [location, setLocation] = useState();
+  const [region, setRegion] = useState(amsterdamLocation);
+
+  // JUST FOR DEMO - use Amsterdam location instead of my current
+  // useEffect(() => {
+  //   async function getLocation() {
+  //     let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  //     if (status !== 'granted') {
+  //       alert('Permission to access location was denied');
+  //     }
+  
+  //     // TODO: use Location.watchPositionAsync()
+  //     let currentLocation = await Location.getCurrentPositionAsync({
+  //       accuracy: Location.Accuracy.BestForNavigation
+  //     });
+
+  //     setLocation({ 
+  //       latitude: currentLocation.coords.latitude,
+  //       longitude: currentLocation.coords.longitude
+  //     });
+  //   }
+
+  //   if(!location) {
+  //     getLocation();
+  //   }
+  // });
+
   return (
-    <View style={styles.container}>
-      {parkingList.map(parking => <MapMarker key={parking.id} parking={parking} onParkingSelect={onParkingSelect} />)}
+    <View>
+      <MapView style={{ width: '100%', height: '100%' }} region={region}>
+        {parkingList.map(parking => (
+          <Marker
+            key={parking.id}
+            identifier={parking.id.toString()}
+            onPress={onParkingSelect}
+            pinColor='red'
+            coordinate={parking.location}
+          />
+          )
+        )}
+      </MapView>
+      <View style={styles.statusBar}></View>
     </View>
   );
 }
@@ -54,8 +94,9 @@ function MapContainer(props: ScreenProps) {
 
   return <Map 
     parkingList={parkingList} 
-    onParkingSelect={parking => {
-      selectParking(parking.id);
+    onParkingSelect={e => {
+      const parkingId = Number(e.nativeEvent.id);
+      selectParking(parkingId);
       navigation.navigate('ParkingDetails');
     }} 
   />
