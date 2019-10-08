8f25c6d32f1f9a1f403784ab8aa16fc17caf4fd2
Sergey Onufrienko <sergey@onufrienko.com>
2019-10-08 16:16:12 +0300

    Add types to Redux Store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,14 @@ import Amplify, { Analytics } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import awsconfig from '../../aws-exports'
+import thunk, { ThunkMiddleware } from 'redux-thunk';
+import awsconfig from '../../aws-exports';
 import Navigation from './Navigation';
-import reducers from '../reducers'
+import reducers from '../reducers';
 import * as actions from '../actions';
+import { AppState, Action } from '../types';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk as ThunkMiddleware<AppState, Action, string>));
 
 Amplify.configure(awsconfig);
 Analytics.configure({ disabled: true });
